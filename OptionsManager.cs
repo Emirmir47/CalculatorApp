9555681d690e4ab8c5bf5721ceff95aa47b5ba7e
Workings <selimsebaydin@gmail.com>
2024-01-10 08:57:09 +0200

    amigo done

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -1,1 +1,44 @@
-﻿//dsdsd
+﻿
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "d - Divide",
+            "sabs - Subtract Abs",
+            "dr - Divide Remainer",
+            "ex - Exit"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} : {b} = {a / b}");
+        }
+        public static void SubtractAbs(double a, double b)
+        {
+            Console.WriteLine($"|{a} - {b}| = {Math.Abs(a - b)}");
+        }
+        public static void DivideReminder(double a, double b)
+        {
+            Console.WriteLine($"{a} % {b} = {a % b}");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,1 +1,82 @@
-﻿
+﻿using System;
+using System.Globalization;
+
+namespace GitExercise
+{
+    public class Startup
+    {
+        private static bool CleckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+
+        private const string Password = "abcd1234";
+        public static void Main()
+        {
+            bool isAuthorized = CleckCredentials();
+
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Console Calculator App");
+                Console.WriteLine(new string('-', 15));
+
+                Console.Write("a = ");
+                double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.Write("b = ");
+                double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.WriteLine("Choose one from the listed options:");
+                foreach (string option in OptionsManager.OptionsList)
+                {
+                    Console.WriteLine($"\t{option}");
+                }
+
+                Console.Write("Option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "a":
+                        OptionsManager.Add(a, b);
+                        break;
+                    case "s":
+                        OptionsManager.Subtract(a, b);
+                        break;
+                    case "m":
+                        OptionsManager.Multiply(a, b);
+                        break;
+                    case "d":
+                        OptionsManager.Divide(a, b);
+                        break;
+                    case "sabs":
+                        OptionsManager.SubtractAbs(a, b);
+                        break;
+                    case "dr":
+                        OptionsManager.DivideReminder(a, b);
+                        break;
+                    case "ex":
+                        Console.Clear();
+                        Console.WriteLine("Goodbye");
+                        Console.ReadKey(intercept: true);
+                        return;
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(intercept: true);
+            }
+        }
+    }
+}
